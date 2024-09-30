f1a709c649b03616fa1ed526ee7896ef8bd0ca92
ErfanHemmesi <erfannhemmesi@gmail.com>
2024-09-30 11:29:38 +0330

    Revert "forTest2"
    This reverts commit 7a77689d273fb12afe5593610ecd90fc9d085adf.
    and for learning github

--- tasGame/fortest.js
+++ tasGame/fortest.js
@@ -1,2 +1,10 @@
-console.log('object');
-console.log('object');
+array1 =Array[0,1,2,3]
+array2 = Array[4,5,6,7]
+
+x=1;
+
+let skjan= `array${x}`
+
+skjan.array.forEach(element => {
+    console.log(element);
+});
